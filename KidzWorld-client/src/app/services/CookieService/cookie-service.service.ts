87559f4a4b840b934af2e522584ff591103a6081
Ayush9535 <ghodkeayush22@gmail.com>
2024-12-19 16:28:25 +0530

    updated cookie config

--- KidzWorld-client/src/app/services/CookieService/cookie-service.service.ts
+++ KidzWorld-client/src/app/services/CookieService/cookie-service.service.ts
@@ -10,7 +10,7 @@ export class CookieServiceService {
   setCookie(name: string, value: string, days: number) {
     const expires = new Date();
     expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
-    document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; secure; HttpOnly;`;
+    document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; secure; HttpOnly; SameSite=None`;
   }
 
   getCookie(name: string): string | null {
