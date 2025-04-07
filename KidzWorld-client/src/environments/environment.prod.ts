0b9158d174f43d7a9156e6bdd401e9e5b356a70d
Ayush Ghodke <144110688+Ayush9535@users.noreply.github.com>
2025-04-07 16:14:04 +0530

    Update environment.prod.ts

--- KidzWorld-client/src/environments/environment.prod.ts
+++ KidzWorld-client/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://kidzworld-backend.onrender.com',  
+    // apiUrl: 'https://kidzworld-backend.onrender.com',
+    apiUrl: 'https://practiceprojectangularecommerc-production.up.railway.app',
     featureFlag: false                 
 };
   
